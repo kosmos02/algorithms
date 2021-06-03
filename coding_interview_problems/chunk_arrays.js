37d1d303b29764fee4516406bb25035adcd662a4
kosmos02 <alexandrgabe@gmail.com>
2021-06-03 10:55:41 -0600

    added more solutions to chunk arrays

--- coding_interview_problems/chunk_arrays.js
+++ coding_interview_problems/chunk_arrays.js
@@ -44,3 +44,16 @@ module.exports = chunk;
 //     }
 //     return chunked
 // }
+
+// function chunk(array, size){
+    //     const chunked = []
+    
+    //     let index = 0
+    
+    //     while (index < array.length){
+    //         chunked.push(array.slice(index, index + size))
+    //         index += size
+    //     }
+    
+    //     return chunked
+    // }
