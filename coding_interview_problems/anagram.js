4331240eebde221c174eeb732a5eea6ca4ba9116
kosmos02 <alexandrgabe@gmail.com>
2021-06-03 12:30:11 -0600

    anagram

---
+++ coding_interview_problems/anagram.js
@@ -0,0 +1,52 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+    const fixedStringA = stringA.replace(/[^\w]/g, "").toLowerCase()
+    const fixedStringB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+    let charMapA = {}
+    let charMapB= {}
+
+    fixedStringA.split('').map(char => {
+        if (!charMapA[char]){
+            charMapA[char] = 1
+        } else {
+            charMapA[char]++
+        }
+    })
+
+    fixedStringB.split('').map(char => {
+        if (!charMapB[char]){
+            charMapB[char] = 1
+        } else {
+            charMapB[char]++
+        }
+    })
+
+    if (Object.keys(charMapA).length != Object.keys(charMapB).length){
+        return false
+    } else {
+        let counter = 0
+        for (let char in charMapA){
+            if (charMapA[char] != charMapB[char]){
+                return false
+            }
+        }
+        return true
+    }
+
+}
+
+console.log(anagrams("HELLO THERE!!!!!", "hello there"))
+
+module.exports = anagrams;
+
+// word.replace(/[^\w]/g, "").toLowerVase()
