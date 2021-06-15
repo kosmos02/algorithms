4d320c0341f10f3370cead303ebe0a50949e8b4f
kosmos02 <alexandrgabe@gmail.com>
2021-06-15 10:06:30 -0600

    vowels

---
+++ coding_interview_problems/vowels.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+    let vowelCount = 0
+
+    const lowerString = str.toLowerCase()
+
+    for (let i = 0; i < str.length; i++){
+        if (lowerString[i] === 'a' || lowerString[i] === 'e' || lowerString[i] === 'i' || lowerString[i] === 'o' || lowerString[i] === 'u'){
+            vowelCount++
+        } 
+    }
+    return vowelCount
+}
+
+module.exports = vowels;
